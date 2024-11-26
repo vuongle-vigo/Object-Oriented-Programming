d2f11ed426bf9e1099cdaff3bff29e6aff256b26
Vigo0x1 <vuongle.vigo@gmail.com>
2024-11-26 17:18:35 +0700

    push code passing parameter

--- Lab03/AimsProject/disc/DigitalVideoDisc.java
+++ Lab03/AimsProject/disc/DigitalVideoDisc.java
@@ -35,6 +35,10 @@ public class DigitalVideoDisc {
         return title;
     }
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
     public String getCategory() {
         return category;
     }
